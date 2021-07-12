9e47dd51ad1d2fef125bc58c92a3abdaf3cc774d
5t4r8ucK <39564849+5t4r8ucK@users.noreply.github.com>
2021-07-12 20:19:40 +0200

    Adjusted grid layout. Added bg colour.

--- src/components/clock.js
+++ src/components/clock.js
@@ -1,22 +1,26 @@
 import styled from 'styled-components';
+import * as variables from './variables.js';
 import Controls from './controls.js';
 import Title from './title.js';
 import Timer from './timer.js';
 
 const ClockWrapper = styled.article`
   // Display and Box Model
+  background: ${variables.colors.gradient.session};
   grid-template-areas:
     'title'
     'timer'
     'controls';
+  grid-template-rows: auto 1fr auto;
   display: grid;
   height: 100vh;
   padding: 5rem;
   width: 100vw;
+  // Typography
+  color: ${variables.colors.color.white.w_100};
 `;
 
 const Clock = () => {
-
   return (
     <ClockWrapper id='clock'>
       <Title id='title'>

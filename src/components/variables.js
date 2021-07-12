5321961d55063104ed13a32113fd044677f3aeed
5t4r8ucK <39564849+5t4r8ucK@users.noreply.github.com>
2021-07-12 20:06:13 +0200

    Added variables

---
+++ src/components/variables.js
@@ -0,0 +1,50 @@
+// Fonts
+export const fonts = {
+  'stack': {
+    'slabSerif': '"Roboto Slab", serif',
+    'sansSerif': '"Lato", sans-serif',
+    'serif': '"Prata", serif',
+    'icons': 'Font Awesome 5 Free',
+  },
+  'sizes': {
+    'h1': '11.089rem',
+    'h2': '6.854rem',
+    'h3': '4.236rem',
+    'h4': '2.618rem',
+    'h5': '1.618rem',
+    'h6': '1rem',
+    'small': '0.618rem',
+  },
+}
+
+// Colors
+export const colors = {
+  'color': {
+    'white':
+    {
+      'w_100': 'rgba(255, 255, 255, 1)',
+      'w_10': 'rgba(255, 255, 255, 0.1)',
+    },
+    'black': {
+      'b_100': 'rgba(0, 0, 0, 1)',
+      'b_10': 'rgba(0, 0, 0, 0.1)',
+    },
+    'red': 'rgba(255,0,0,1)',
+    'orange': 'rgba(255,0,0,1)',
+    'green': 'rgba(1,213,0,1)',
+    'turquoise': 'rgba(32,231,212,1)',
+  },
+  'gradient': {
+    'session': 'linear-gradient(0deg, rgba(255,0,0,1) 0%, rgba(255,64,0,1) 100%);',
+    'break': 'linear-gradient(0deg, rgba(1,213,0,1) 0%, rgba(32,231,212,1) 100%);',
+  }
+}
+
+// Measurements
+export const sizes = {
+  'gapWidth': '1rem', // 16 px
+  'hairlineWidth': '0.0625rem', // 1 px
+  'borderWidth': '0.125rem', // 2 px
+  'buttonBorderRadius': '0.3125rem', // 5 px
+  'column_width': '4rem', // 64 px
+}

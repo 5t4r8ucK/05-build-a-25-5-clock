3620d6d50bbfe9f452a42d676dfb4d78810b452d
5t4r8ucK <39564849+5t4r8ucK@users.noreply.github.com>
2021-07-12 20:07:58 +0200

    Changed title to a header element, added styles

--- src/components/title.js
+++ src/components/title.js
@@ -1,8 +1,13 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
+import * as variables from './variables.js';
+import StyledHeader from './styledHeader.js';
 
-const Title = styled.section`
-  // Display and Box Model
-  text-align: center;
-`;
-
+const Title = styled(StyledHeader)`
+  &#title {
+    // Typography
+    font-size: ${variables.fonts.sizes.h4};
+    text-shadow: 0.03em 0.03em ${variables.colors.color.black.b_10};
+    text-transform: uppercase;
+  }
+`
 export default Title;

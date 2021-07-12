364e986e3335d516b951f2eb79c024ce15b31861
5t4r8ucK <39564849+5t4r8ucK@users.noreply.github.com>
2021-07-12 20:06:51 +0200

    Added a general header style

---
+++ src/components/styledHeader.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import * as variables from './variables.js';
+
+const StyledHeader = styled.header`
+  // Typography
+  text-align: center;
+  letter-spacing: 0.1em;
+`
+
+export default StyledHeader;

3dc3f16a839400442d06b51c1c386acf67edff47
5t4r8ucK <39564849+5t4r8ucK@users.noreply.github.com>
2021-07-16 17:54:42 +0200

    Added styles for the title component.

--- src/components/title.js
+++ src/components/title.js
@@ -4,6 +4,12 @@ import StyledHeader from './styledHeader.js';
 
 const Title = styled(StyledHeader)`
   &#title {
+    // Display and Box Model
+    justify-self: center;
+    max-width: 50rem;
+    border-width: 0 0 ${variables.sizes.borderWidth} 0;
+    border-style: solid;
+    border-bottom-color: ${variables.colors.color.white.w_10};
     // Typography
     font-size: ${variables.fonts.sizes.h4};
     text-shadow: 0.03em 0.03em ${variables.colors.color.black.b_10};

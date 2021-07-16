171df76063e45e1fd55d65e4c48d0f44d1bb1816
5t4r8ucK <39564849+5t4r8ucK@users.noreply.github.com>
2021-07-16 18:34:50 +0200

    Logic for starting pausing, resetting and volume.

--- src/components/playback.js
+++ src/components/playback.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import * as variables from './variables.js';
 import StyledButton from './styledButton.js';
 import ControlsContainer from './controlsContainer';
+import Audio from './audio';
 
 const PlaybackControls = styled(ControlsContainer)`
   &#playback-controls {
@@ -30,6 +31,8 @@ const PlaybackButton = styled(StyledButton)`
   }
 `
 
+let newTimer;
+
 const Playback = ({initialClockState, setTimerLabel, setTimerLength, setTimerType, initialControlsState, breakLength, setBreakLength, isTimerPaused, setIsTimerPaused, sessionLength, setSessionLength}) => {
 
   const initialPlaybackState = {
@@ -37,15 +40,122 @@ const Playback = ({initialClockState, setTimerLabel, setTimerLength, setTimerTyp
   }
   const [playbackIcon, setPlaybackIcon] = useState(initialPlaybackState.playbackIcon);
 
+  let audio = useRef(null);
+
+  const startTimer = (durationOne, durationTwo) => {
+    durationOne = durationOne * 60; // total secs = # of mins * 60 secs
+    durationTwo = durationTwo * 60; // total secs = # of mins * 60 secs
+
+    let duration = durationOne;
+    let minutes;
+    let seconds;
+    let interval;
+    let isInSession = true;
+    let timer = {};
+
+    timer.resume = () => {
+      interval = setInterval(() => {
+        duration = duration - 1;
+        minutes = parseInt(duration / 60, 10);
+        seconds = parseInt(duration % 60, 10);
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+        if (duration < 0) {
+          isInSession = !isInSession;
+          if (isInSession) {
+            duration = durationOne; // reset to session timer
+            setTimerLabel('currently in a session');
+            setTimerType('session');
+          } else {
+            duration = durationTwo; // reset to break timer
+            setTimerLabel('currently on a break');
+            setTimerType('break');
+            audio.current.play();
+          }
+        }
+        const display = minutes + ':' + seconds; // concat mins and secs to '00:00' format
+        setTimerLength(display);
+      }, 1000);
+    }
+    timer.pause = () => {
+      clearInterval(interval);
+    }
+    return timer;
+  }
+
+  const stopTimer = (timer) => {
+    timer.pause();
+  }
+
+  const resumeTimer = (timer) => {
+    timer.resume();
+  }
+
+  const handleClick = (action) => {
+    switch (action) {
+      case 'reset':
+        if (newTimer !== undefined) {
+          stopTimer(newTimer);
+        }
+        newTimer = undefined;
+        setPlaybackIcon(initialPlaybackState.playbackIcon);
+        setIsTimerPaused(initialControlsState.isTimerPaused);
+        setBreakLength(initialControlsState.break.length);
+        setSessionLength(initialControlsState.session.length);
+        setTimerLabel(initialClockState.timer.label);
+        setTimerLength(initialClockState.timer.length);
+        setTimerType(initialClockState.timer.type);
+        audio.current.pause();
+        audio.current.currentTime = 0;
+        break;
+      case 'start_stop':
+        if (newTimer === undefined) {
+          newTimer = startTimer(sessionLength, breakLength);
+        }
+        if (isTimerPaused) {
+          resumeTimer(newTimer);
+          setPlaybackIcon('fas fa-pause');
+          setIsTimerPaused(false);
+        } else {
+          stopTimer(newTimer);
+          setPlaybackIcon('fas fa-play');
+          setIsTimerPaused(true);
+        }
+        break;
+      case 'volume':
+        audio.current.pause();
+        audio.current.currentTime = 0;
+        break;
+      default:
+        break;
+    }
+  }
+
   return (
     <PlaybackControls id='playback-controls'>
+      <PlaybackButton
+        id='start_stop'
+        onClick={() => handleClick('start_stop')}
+      >
         <i className={playbackIcon}></i>
       </PlaybackButton>
-      <PlaybackButton id='reset'>
-        <i className="fas fa-redo"></i>
+      <PlaybackButton
+        id='reset'
+        onClick={() => handleClick('reset')}
+      >
+        <i className='fas fa-redo'></i>
       </PlaybackButton>
-      <PlaybackButton id='volume'>
-        <i className="fas fa-volume-up"></i>
+      <PlaybackButton
+        id='volume'
+        onClick={() => handleClick('volume')}
+      >
+        <i className='fas fa-volume-up'></i>
+        <Audio
+          id='beep'
+          preload="auto"
+          src='./sounds/Daybreak.mp3'
+          ref={audio}
+        />
       </PlaybackButton>
     </PlaybackControls>
   );

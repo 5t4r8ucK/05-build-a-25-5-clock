8e93627723cfe844d7722be406a3f4f62e8cc552
5t4r8ucK <39564849+5t4r8ucK@users.noreply.github.com>
2021-07-16 18:33:38 +0200

    Added classes to change bg gradient.

--- src/components/clock.js
+++ src/components/clock.js
@@ -1,24 +1,59 @@
 import React, {useState} from 'react';
 import styled from 'styled-components';
-import * as variables from './variables.js';
-import Controls from './controls.js';
-import Title from './title.js';
-import Timer from './timer.js';
+import * as variables from './variables';
+import StyledArticle from './styledArticle';
+import Controls from './controls';
+import Title from './title';
+import Timer from './timer';
 
-const ClockWrapper = styled.article`
-  // Display and Box Model
-  background: ${variables.colors.gradient.session};
-  grid-template-areas:
-    'title'
-    'timer'
-    'controls';
-  grid-template-rows: auto 1fr auto;
-  display: grid;
-  height: 100vh;
-  padding: 5rem;
-  width: 100vw;
-  // Typography
-  color: ${variables.colors.color.white.w_100};
+const ClockArticle = styled(StyledArticle)`
+  &#clock {
+    // Position
+    // Display and Box Model
+    background: linear-gradient(
+      0deg,
+      ${variables.colors.color.turquoise},
+      ${variables.colors.color.green},
+      ${variables.colors.color.orange},
+      ${variables.colors.color.red}
+    );
+    background-size: 100% 400%;
+    grid-template-areas:
+      'title'
+      'timer'
+      'controls';
+    grid-template-rows: auto 1fr auto;
+    // Typography
+    color: ${variables.colors.color.white.w_100};
+    // Other
+    animation-fill-mode: forwards;
+  }
+  &.session {
+    // Display and Box Model
+    animation-name: transitionToSession;
+    animation-duration: 1s;
+  }
+  &.break {
+    // Display and Box Model
+    animation-name: transitionToBreak;
+    animation-duration: 1s;
+  }
+  @keyframes transitionToBreak {
+    from {
+      background-position: top;
+    }
+    to {
+      background-position: bottom;
+    }
+  }
+  @keyframes transitionToSession {
+    from {
+      background-position: bottom;
+    }
+    to {
+      background-position: top;
+    }
+  }
 `;
 
 const Clock = () => {

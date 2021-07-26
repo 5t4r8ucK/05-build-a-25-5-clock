a546b8c08c7ecb950160ee28b0d0b04f5536b8ec
5t4r8ucK <39564849+5t4r8ucK@users.noreply.github.com>
2021-07-26 18:24:15 +0200

    Made title wider to accomodate bigger font sizes

--- src/components/title.js
+++ src/components/title.js
@@ -5,11 +5,11 @@ import StyledHeader from './styledHeader.js';
 const Title = styled(StyledHeader)`
   &#title {
     // Display and Box Model
-    justify-self: center;
-    max-width: 50rem;
     border-width: 0 0 ${variables.sizes.borderWidth} 0;
     border-style: solid;
     border-bottom-color: ${variables.colors.color.white.w_10};
+    justify-self: center;
+    max-width: 80rem;
     // Typography
     font-size: ${variables.fonts.sizes.h4};
     text-shadow: 0.03em 0.03em ${variables.colors.color.black.b_10};
